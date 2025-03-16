3d4a6826c776bc43908eca068eff9917548e63df
Simin Kou <132246826+SiminKoux@users.noreply.github.com>
2025-03-16 14:38:42 +1300

    Update inverse_loss.py

--- losses/inverse_loss.py
+++ losses/inverse_loss.py
@@ -9,12 +9,10 @@ class InverseLoss(nn.Module):
         super().__init__()
         self.weight = weight
 
-    def forward(self, batch_in, batch_out):
+    def forward(self, batch_out):
         gt_xyzt = batch_out["gt_xyzt"]
-        # print("gt_xyzt:", gt_xyzt)
         pred_xyzt_f = batch_out["inverse_xyzt_f"]
         pred_xyzt_b = batch_out["inverse_xyzt_b"]
-        # print("pred_xyzt:", pred_xyzt)
         if self.weight <= 0:
             return None
         inverse_loss_f = F.l1_loss(pred_xyzt_f, gt_xyzt)
